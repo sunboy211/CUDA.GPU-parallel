2bdb2853a3eed183c3a37a8613f05812443b81bd
sunboy211 <70102284+sunboy211@users.noreply.github.com>
2024-05-29 20:21:49 +0800

    Update imflipG.cu

--- imflipG.cu
+++ imflipG.cu
@@ -18,3 +18,17 @@ uch *GPUImg,*GPUCopyImg,*GPUResult;
 //cudaGetDeviceCount() 一共几个GPU
 //cudaSetDevice() 选择哪个GPU
 //cudaGetDeviceProperties GPU参数
+//GPUProp.maxGridSize[0],GPUProp.maxGridSize[1],GPUProp.maxGridSize[2]分别表示x,y,z维度上可启动的最大线程块数
+
+int main(int argc,char** argv){
+  cudaError_t cudaStatus,cudaStatus2;
+  cudaDeviceProp GPUprop;
+  ul SupportedKBlocks,SupportedMBlocks,MaxThrPerBlk;
+  char SupportedBlocks[100];
+
+  int NumGPUs=0; cudaGetDeviceCount(&NumGPUs);
+  if(NumGPUs==0){
+    printf("\nNo cuda device is available.\n"); exit(EXIT_FAILURE);
+  }
+  cudaStatus=NumGPUs(0)
+}
