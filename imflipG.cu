e782c76d888d985c61624df8afbb15604fe91250
sunboy211 <70102284+sunboy211@users.noreply.github.com>
2024-05-29 11:18:56 +0800

    Create imflipG.cu

---
+++ imflipG.cu
@@ -0,0 +1,17 @@
+#include<cuda.h>
+#include<cuda_runtime.h>
+#include<stdio.h>
+#include<stdint.h>
+#include<iostream>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<device_launch_parameters.h>
+
+typedef unsigned int ui;
+typedef unsigned long ul;
+typedef unsigned char uch;
+
+uch *TheImg,*CopyImg;
+uch *GPUImg,*GPUCopyImg,*GPUResult;
+
