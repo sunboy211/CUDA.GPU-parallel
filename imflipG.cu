965478e9a1de5aa229d74c4b5f08727ed8ab5425
sunboy211 <70102284+sunboy211@users.noreply.github.com>
2024-05-30 11:11:44 +0800

    Update imflipG.cu

--- imflipG.cu
+++ imflipG.cu
@@ -20,6 +20,7 @@ uch *GPUImg,*GPUCopyImg,*GPUResult;
 //cudaGetDeviceProperties GPU参数
 //GPUProp.maxGridSize[0],GPUProp.maxGridSize[1],GPUProp.maxGridSize[2]分别表示x,y,z维度上可启动的最大线程块数
 
+//下面是Nvidia API初始化并查询GPU
 int main(int argc,char** argv){
   cudaError_t cudaStatus,cudaStatus2;
   cudaDeviceProp GPUprop;
@@ -28,7 +29,15 @@ int main(int argc,char** argv){
 
   int NumGPUs=0; cudaGetDeviceCount(&NumGPUs);
   if(NumGPUs==0){
-    printf("\nNo cuda device is available.\n"); exit(EXIT_FAILURE);
+    printf("\n No cuda device is available.\n"); exit(EXIT_FAILURE);
   }
   cudaStatus=cudaSetDevice(0);
+  if(cudaStatus!=cudaSuccess){
+    fprintf(stderr,"cudasetDevice failed!No cuda-capable GPU is installed?");
+    exit(EXIT_FAILURE);
+  }
+  cudaGetDeviceProperties(&GPUprop,0);
+  SupportedKBlocks=(ui)GPUProp.maxGridSize[0] * (ui)GPUProp.maxGridSize[1] * (ui)GPUProp.maxGridSize[2] / 1024;
+  SupportedMBlocks=SupportedKBlocks / 1024;
+  MaxThrPerBlk=(ui)GPUProp.maxThreadsPerBlock;
 }
